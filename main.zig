c6016d1ac383143ba3279b9f2c5977c8fb9d2a47
Alex Miranda <alexmirandamoraes@gmail.com>
2024-12-01 22:23:43 +0100

    Create zig project folder structure

---
+++ main.zig
@@ -0,0 +1,25 @@
+pub const day01 = @import("day01/main.zig");
+pub const day02 = @import("day02/main.zig");
+pub const day03 = @import("day03/main.zig");
+pub const day04 = @import("day04/main.zig");
+pub const day05 = @import("day05/main.zig");
+pub const day06 = @import("day06/main.zig");
+pub const day07 = @import("day07/main.zig");
+pub const day08 = @import("day08/main.zig");
+pub const day09 = @import("day09/main.zig");
+pub const day10 = @import("day10/main.zig");
+pub const day11 = @import("day11/main.zig");
+pub const day12 = @import("day12/main.zig");
+pub const day13 = @import("day13/main.zig");
+pub const day14 = @import("day14/main.zig");
+pub const day15 = @import("day15/main.zig");
+pub const day16 = @import("day16/main.zig");
+pub const day17 = @import("day17/main.zig");
+pub const day18 = @import("day18/main.zig");
+pub const day19 = @import("day19/main.zig");
+pub const day20 = @import("day20/main.zig");
+pub const day21 = @import("day21/main.zig");
+
+test {
+    @import("std").testing.refAllDecls(@This());
+}
