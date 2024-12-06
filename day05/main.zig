fe09e6f5b56bae3cdd82dc1325ff8b70013f72c3
Alex Miranda <alexmirandamoraes@gmail.com>
2024-12-06 15:34:58 +0100

    day05 part 2

--- day05/main.zig
+++ day05/main.zig
@@ -26,6 +26,10 @@ pub fn main() !void {
 
     const answer_p1 = try solve1(ally, input_file.reader());
     try stdout.print("Part 1: {d}\n", .{answer_p1});
+
+    try input_file.seekTo(0);
+    const answer_p2 = try solve2(ally, input_file.reader());
+    try stdout.print("Part 2: {d}\n", .{answer_p2});
 }
 
 fn solve1(ally: std.mem.Allocator, reader: anytype) !u32 {
@@ -90,6 +94,91 @@ fn solve1(ally: std.mem.Allocator, reader: anytype) !u32 {
     return sum;
 }
 
+fn solve2(ally: std.mem.Allocator, reader: anytype) !u32 {
+    const Rule = struct { left: u8, right: u8 };
+
+    var br = std.io.bufferedReader(reader);
+    var r = br.reader();
+
+    // first we read all the rules and create a permitted: a < b; and violation rule: b < a
+    const rules = blk: {
+        var set = std.AutoHashMap(Rule, std.math.Order).init(ally);
+        errdefer set.deinit();
+        var buf: [6]u8 = undefined;
+        while (true) {
+            const read = try r.readUntilDelimiter(&buf, '\n');
+            if (read.len == 0) break;
+            const a = try std.fmt.parseInt(u8, read[0..2], 10);
+            const b = try std.fmt.parseInt(u8, read[3..], 10);
+            try set.put(.{ .left = a, .right = b }, .lt);
+            try set.put(.{ .left = b, .right = a }, .gt);
+        }
+        break :blk &set;
+    };
+    defer rules.deinit();
+
+    var line = try std.ArrayList(u8).initCapacity(ally, 100);
+    defer line.deinit();
+
+    var pages = std.ArrayList(u8).init(ally);
+    defer pages.deinit();
+
+    var sum: u32 = 0;
+    outer: while (true) {
+        defer line.clearRetainingCapacity();
+        defer pages.clearRetainingCapacity();
+
+        // read each line
+        r.readUntilDelimiterArrayList(&line, '\n', line.capacity) catch |err| {
+            switch (err) {
+                error.EndOfStream => break,
+                else => return err,
+            }
+        };
+
+        // check if it's an empty line
+        if (line.items.len == 0) continue :outer;
+
+        // we know that each page number is two digit long, so it's safe to use a WindowIterator
+        var incorrectly_ordered = false;
+        var it = std.mem.window(u8, line.items, 2, 3);
+        while (it.next()) |tok| {
+            if (tok.len == 0) continue :outer;
+            const a = try std.fmt.parseInt(u8, tok, 10);
+
+            // because we need to count only the incorrectly ordered updates, we need to do this gimmickry
+            if (!incorrectly_ordered) {
+                for (pages.items) |b| {
+                    // if a violation is found, we attempt to fix it
+                    if (rules.get(.{ .left = b, .right = a })) |order| {
+                        if (order == .gt) {
+                            incorrectly_ordered = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            try pages.append(a);
+        }
+
+        assert(pages.items.len > 0);
+        if (incorrectly_ordered) {
+            const S = struct {
+                pub fn lessThan(ctx: @TypeOf(rules), a: u8, b: u8) bool {
+                    const cmp = ctx.get(.{ .left = a, .right = b }) orelse .eq;
+                    return switch (cmp) {
+                        .gt => false,
+                        else => true,
+                    };
+                }
+            };
+            std.sort.heap(u8, pages.items, rules, S.lessThan);
+            const middle = pages.items[pages.items.len / 2];
+            sum += middle;
+        }
+    }
+    return sum;
+}
 test "part 1" {
     var fbs = std.io.fixedBufferStream(example);
     const answer = try solve1(testing.allocator, fbs.reader());
@@ -97,5 +186,7 @@ test "part 1" {
 }
 
 test "part 2" {
-    return error.SkipZigTest;
+    var fbs = std.io.fixedBufferStream(example);
+    const answer = try solve2(testing.allocator, fbs.reader());
+    try expectEqual(123, answer);
 }
