cb738aba2c7a4b7e8882f43e966fea1bddad18e2
Alex Miranda <alexmirandamoraes@gmail.com>
2025-01-16 02:12:10 +0100

    day14 part 2

--- day14/main.zig
+++ day14/main.zig
@@ -79,6 +79,70 @@ fn Bathroom(comptime width: i32, comptime height: i32) type {
             // print("{d}\n", .{quadrants});
             return @reduce(.Mul, quadrants);
         }
+
+        fn simulate(self: *Self, writer: anytype) !void {
+            var robots = self.robots.move();
+            defer robots.deinit(self.ally);
+            // try robots.ensureTotalCapacity(self.ally, width * height * 2);
+
+            var next = std.AutoHashMapUnmanaged(Robot, void){};
+            defer robots.deinit(self.ally);
+            // try next.ensureTotalCapacity(self.ally, width * height * 2);
+
+            for (1..10_000) |n| {
+                var xs: [width]u8 = .{0} ** width;
+                var ys: [height]u8 = .{0} ** height;
+                var iter = robots.keyIterator();
+                while (iter.next()) |key_ptr| {
+                    var robot = key_ptr.*;
+                    robot.loc.x = @mod(robot.loc.x + robot.vel.x, width);
+                    robot.loc.y = @mod(robot.loc.y + robot.vel.y, height);
+                    xs[@as(u32, @bitCast(robot.loc.x))] += 1;
+                    ys[@as(u32, @bitCast(robot.loc.y))] += 1;
+                    try next.put(self.ally, robot, {});
+                    robots.removeByPtr(key_ptr);
+                }
+                std.mem.swap(std.AutoHashMapUnmanaged(Robot, void), &robots, &next);
+
+                // detect frame around x-mas tree
+                var score: usize = 0;
+                for (xs ++ ys) |x| {
+                    if (x >= 31) {
+                        score += x;
+                    }
+                }
+
+                if (score >= 31 * 4) {
+                    var found: Self = .{ .ally = self.ally, .robots = robots.move() };
+                    defer found.deinit();
+                    try writer.print("N={d}\n", .{n});
+                    try writer.print("{?}", .{found});
+                    break;
+                }
+            }
+        }
+
+        pub fn format(self: Self, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
+            const size = width * height + height;
+            var tiles: [size]u8 = .{'.'} ** size;
+            var iter = self.robots.keyIterator();
+            while (iter.next()) |key_ptr| {
+                const robot = key_ptr.*;
+                const pos: u32 = @bitCast(robot.loc.y * (width + 1) + robot.loc.x);
+                tiles[pos] = switch (tiles[pos]) {
+                    '.' => '1',
+                    '1'...'8' => |c| c + 1,
+                    '9', '#' => '#',
+                    else => unreachable,
+                };
+            }
+            var slide: u32 = @bitCast(width);
+            for (0..height) |_| {
+                tiles[slide] = '\n';
+                slide += width + 1;
+            }
+            try writer.print("{s}", .{tiles});
+        }
     };
 }
 
@@ -111,15 +175,14 @@ pub fn main() !void {
 
     const answer_p1 = bathroom.calculateSafetyFactor();
     try stdout.print("Part 1: {d}\n", .{answer_p1});
+
+    try bathroom.simulate(stdout);
 }
 
 test "part 1" {
     var bathroom = try Bathroom(11, 7).initParse(testing.allocator, example);
     defer bathroom.deinit();
     const safety_factor = bathroom.calculateSafetyFactor();
+    print("{?}\n", .{bathroom});
     try expectEqual(12, safety_factor);
-}
-
-test "part 2" {
-    return error.SkipZigTest;
 }
